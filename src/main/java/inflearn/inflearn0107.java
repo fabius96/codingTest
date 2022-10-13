106a982941ff073e0ac115a51c38eeb4aadd7b7a
KORJaeyoungKim <jy0511_@naver.com>
2022-10-13 09:39:52 +0900

    자바 알고리즘 문제풀이 1-7번 해결

--- src/main/java/inflearn/inflearn0107.java
+++ src/main/java/inflearn/inflearn0107.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 
 public class inflearn0107 {
     public String solution(String str) {
-        String answer= "";
-        for (int i = 0; i < str.length(); i++) {
-            if (str.indexOf(str.charAt(i)) == i) answer += str.charAt(i);
+        String answer= "YES";
+        char[] charArray = str.toUpperCase().toCharArray();
+        int a = charArray.length;
+        for (int i = 0; i < str.length()/2; i++) {
+            System.out.println(charArray[i]);
+            System.out.println(charArray[a-1]);
+            if (charArray[i] != charArray[a-1]) answer = "NO";
+            a--;
         }
         return answer;
     }
