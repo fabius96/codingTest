3642cf375532a3dab41fd3d5b8ede96ecb71fdc6
KORJaeyoungKim <jy0511_@naver.com>
2022-10-14 17:22:23 +0900

    프로그래머스 120817번 배열의 평균값 문제 해결

---
+++ src/main/java/programmers/Programmers120817.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class Programmers120817 {
+    public double solution(int[] numbers) {
+        double answer = 0;
+
+        for(int x : numbers) {
+            answer += x;
+        }
+
+       answer = answer/numbers.length;
+
+        return answer;
+    }
+
+    public static void main(String[] arg) {
+        Programmers120817 test = new Programmers120817();
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(test.solution(numbers));
+    }
+}
