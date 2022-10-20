d59da7ff2019b0e973fb8ecbd46f3fcde800e126
KORJaeyoungKim <jy0511_@naver.com>
2022-10-20 10:03:00 +0900

    프로그래머스 120826 특정 문자 제거하기 리팩터링

--- src/main/java/programmers/Programmers120826.java
+++ src/main/java/programmers/Programmers120826.java
@@ -3,13 +3,7 @@ package programmers;
 /* 특정 문자 제거하기 */
 public class Programmers120826 {
     public String solution(String my_string, String letter) {
-        String answer = "";
-        for(int i = 0; i < my_string.length(); i++){
-            if(!letter.equals(String.valueOf(my_string.charAt(i)))){
-                answer+=my_string.charAt(i);
-            }
-        }
-        return answer;
+        return my_string.replace(letter, "");
     }
 
 
