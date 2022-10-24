86360ddbd47c0cae429f9fe2f555a1bceb50a348
KORJaeyoungKim <jy0511_@naver.com>
2022-10-24 20:33:54 +0900

    프로그래머스 120911 문자열 정렬하기 (2) 리팩터링

--- src/main/java/programmers/Programmers120911.java
+++ src/main/java/programmers/Programmers120911.java
@@ -5,16 +5,9 @@ import java.util.Arrays;
 /* 문자열 정렬하기 (2) */
 public class Programmers120911 {
     public String solution(String my_string) {
-        StringBuilder answer = new StringBuilder();
-        char[] charArray = my_string.toCharArray();
-        for (int i = 0; i < my_string.length(); i++) {
-            if (Character.isUpperCase(charArray[i])) charArray[i] = Character.toLowerCase(charArray[i]);
-        }
-        Arrays.sort(charArray);
-        for(char c : charArray){
-            answer.append(c);
-        }
-        return answer.toString();
+        char[] answer = my_string.toLowerCase().toCharArray();
+        Arrays.sort(answer);
+        return new String(answer);
     }
 
     public static void main(String[] arg) {
