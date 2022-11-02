8b19db118d03794ce1f13b38e2a93d18cb4f8c19
KORJaeyoungKim <jy0511_@naver.com>
2022-11-02 09:54:08 +0900

    프로그래머스 120888 중복된 문자 제거 문제 리팩터링

--- src/main/java/programmers/Programmers120888.java
+++ src/main/java/programmers/Programmers120888.java
@@ -3,17 +3,10 @@ package programmers;
 /* 중복된 문자 제거 */
 public class Programmers120888 {
     public String solution(String my_string) {
-        String answer = String.valueOf(my_string.charAt(0));
+        String answer = "";
 
         for (int i = 0; i < my_string.length(); i++) {
-            boolean exist = false;
-            for (int j = 0; j < answer.length(); j++) {
-                if (my_string.charAt(i) == answer.charAt(j)) {
-                    exist = true;
-                    break;
-                }
-            }
-            if (!exist) answer += String.valueOf(my_string.charAt(i));
+            if (i == my_string.indexOf(my_string.charAt(i))) answer += my_string.charAt(i);
         }
         return answer;
     }
