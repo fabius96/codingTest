3bf5a507b9a38bef8ef5eec292685c987845a0e5
KORJaeyoungKim <jy0511_@naver.com>
2022-11-08 10:02:19 +0900

    자바 알고리즘 문제풀이 2-9번 격자판 최대합 문제 해결

---
+++ src/main/java/inflearn/inflearn0209.java
@@ -0,0 +1,46 @@
+package inflearn;
+
+import java.util.Scanner;
+
+/* 격자판 최대합 */
+public class inflearn0209 {
+    public int solution(int n, int[][] arr) {
+        int answer = Integer.MIN_VALUE;
+        int sum1, sum2;
+
+        for (int i = 0; i < n; i++) {
+            sum1 = sum2 = 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+        sum1 = sum2 = 0;
+        for (int i = 0; i < n; i++) {
+
+            sum1 += arr[i][i];
+            sum2 += arr[i][n - i - 1];
+        }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+
+
+        return answer;
+    }
+
+    public static void main(String[] arg) {
+        inflearn0209 T = new inflearn0209();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println(T.solution(n, arr));
+        scanner.close();
+    }
+}
