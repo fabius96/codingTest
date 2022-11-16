428bbecbb5ac82b537e1ff9c651cd8a5c75aaa46
KORJaeyoungKim <jy0511_@naver.com>
2022-11-16 18:33:47 +0900

    백준 11022 A+B - 8 문제 리팩터링(BufferedReader, BufferedWriter 적용)

--- src/main/java/baekjoon/baekjoon11022.java
+++ src/main/java/baekjoon/baekjoon11022.java
@@ -1,19 +1,25 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class baekjoon11022 {
     /* A+B - 8 */
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt();
-        for (int i = 1; i < T+1; i++) {
-            int A = scanner.nextInt();
-            int B = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 1; i < T + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
             int sum = A + B;
-            System.out.println("Case #" + i + ": " + A + " + " + B + " = " + sum);
+            bw.write(("Case #" + i + ": " + A + " + " + B + " = " + sum) + "\n");
         }
-        scanner.close();
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
 
