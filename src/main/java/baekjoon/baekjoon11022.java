b6beb9db3af83b6fba63caac46370fa9d7469f17
KORJaeyoungKim <jy0511_@naver.com>
2022-11-16 17:52:20 +0900

    백준 11022 A+B - 8 문제 해결

---
+++ src/main/java/baekjoon/baekjoon11022.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class baekjoon11022 {
+    /* A+B - 8 */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        for (int i = 1; i < T+1; i++) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            int sum = A + B;
+            System.out.println("Case #" + i + ": " + A + " + " + B + " = " + sum);
+        }
+        scanner.close();
+    }
+}
+
