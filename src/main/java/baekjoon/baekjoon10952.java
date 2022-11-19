02faacadc74599ac0a532bf6c3d801a984134a72
KORJaeyoungKim <jy0511_@naver.com>
2022-11-19 18:49:07 +0900

    백준 10952 A+B - 5 문제 해결

---
+++ src/main/java/baekjoon/baekjoon10952.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class baekjoon10952 {
+    /* A+B - 5 */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        while(true) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            if(A == 0 && B==0) {
+                break;
+            }
+            int sum = A + B;
+            bw.write(sum + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
+
