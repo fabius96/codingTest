ba17de71a395af3c1a8cccb33c134daf52101155
KORJaeyoungKim <jy0511_@naver.com>
2022-12-23 13:48:39 +0900

    자바 알고리즘 문제풀이 03-06 최대 길이 연속부분수열 문제 해결

---
+++ src/main/java/inflearn/inflearn0306.java
@@ -0,0 +1,31 @@
+package inflearn;
+
+import java.util.Scanner;
+
+/* 최대 길이 연속부분수열 */
+public class inflearn0306 {
+    public int solution(int n, int k, int[] arr) {
+        int answer = 0, cnt = 0, lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            if (arr[rt] == 0) cnt++;
+            while (cnt > k) {
+                if (arr[lt] == 0) cnt--;
+                lt++;
+            }
+            answer = Math.max(answer, rt - lt + 1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        inflearn0306 T = new inflearn0306();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.print(T.solution(n, k, arr));
+    }
+}
